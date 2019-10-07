51654f49dd47420dd122bd78d5cd492154a930db
SANJAYTALLOLLI <43472220+sanjaytallolli@users.noreply.github.com>
2019-10-08 02:39:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model= pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    '''
+    For rendering results in HTML GUI
+    '''
+
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+
+    output = round(prediction[0], 2)
+    return render_template('index.html', prediction_text= 'Bike Rental Count Should be {}'. format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
